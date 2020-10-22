bced39ed7b70268269e6c335fe417fed1cb791da
ivanps-ped <61473474+ivanps-ped@users.noreply.github.com>
2020-10-22 16:52:35 +0200

    Final

--- src/index.js
+++ src/index.js
@@ -7,13 +7,28 @@ app.use(bodyParser.json());
 
 let gamer = {
     position: '',
-    alies: '',
+    alias: '',
     name: '',
     surname: '',
     score: 0
 };
 
-let gamerArray = []
+let gamerArray = [
+    gamer = {
+        position: 3,
+        alias: 'mmoñas',
+        name: 'manolo',
+        surname: 'moñas',
+        score: 200
+    },
+        gamer = {
+        position: 1,
+        alias: 'iperez',
+        name: 'ivan',
+        surname: 'perez',
+        score: 1000
+    },
+];
 
 let output = {
     error: false,
@@ -21,66 +36,91 @@ let output = {
     message: ''
 };
 
-app.route('/')
-    .get(function (req, res) {
+function CheckIfExists(nameToCheck, surnameToCheck) {
+    var isRepeated = false;
+    gamerArray.forEach(function (item, index, array) {
+        if (nameToCheck == gamerArray[index].name && surnameToCheck == gamerArray[index].surname) {
+            OutputCase(503);
+            isRepeated = true;
+        }
+    })
+    return isRepeated;
+};
+
+function OutputCase(errorCode) {
+    switch (errorCode) {
+        case 503:
+            output = {
+                error: true,
+                errCode: 503,
+                message: 'The player was alredy existing'
+            };
+            break;
+        case 20:
+            output = {
+                error: false,
+                errCode: 20,
+                message: 'This server works!'
+            };
+            break;
+    }
+};
+
+function IsNull(nameToCheck, surnameToCheck, scoreToCheck) {
+    if (!nameToCheck || !surnameToCheck || !scoreToCheck || scoreToCheck < 0) {
         output = {
             error: true,
-            errCode: 20,
-            message: 'This server works!'
+            errCode: 502,
+            message: 'Required fields: name, surname, score'
         };
+        return true;
+    }
+    return false;
+};
+
+function CreatePlayer(input) {
+    //* If Name AND Surname are original
+    //Get correct inputs
+    gamer = {
+        alias: input.name.charAt(0) + input.surname,
+        position: 1,
+        name: input.name,
+        surname: input.surname,
+        score: input.score,
+    };
+    //Set output to correct fields
+    output = {
+        error: false,
+        errCode: 200,
+        message: 'Player created',
+        output: gamer
+    };
+    //Add new User to "Database" array
+    let newGamer = gamerArray.push(gamer);
+};
+
+app.route('/')
+    .get(function (req, res) {
+        OutputCase(20);
         res.send(output);
     });
 
-app.route('/hola')
-    .get(function (req, res) {
-        res.send("Hi I guess...");
-    });
+app.get('/hola', function (req, res) {
+    res.send("Hi I guess...");
+});
 
 app.route('/gamer')
-    //If enter with Get
     .get(function (req, res) {
-        res.send('You must provide a JSON structure, for that purpose use POST');
+        res.send('You MUST provide a JSON structure, for that purpose use POST');
     })
-    //If enter with Post
+
     .post(function (req, res) {
-        //! In case there's not enought fields
-        if (!req.body.name || !req.body.surname || !req.body.score) {
-            output = {
-                error: true,
-                errCode: 502,
-                message: 'Required fields: name, surname, score'
-            };
-        }
-        //If correct fields
-        else {
-            //* If Name AND Surname are original
-            //Get correct inputs
-            gamer = {
-                alies: req.body.name.charAt(0) + req.body.surname,
-                position: 1,
-                name: req.body.name,
-                surname: req.body.surname,
-                score: req.body.score,
-            };
-            //Set output to correct fields
-            output = {
-                error: false,
-                errCode: 200,
-                message: 'Player created',
-                output: gamer
-            };
-            //! If Name and Surname is repeated
-            gamerArray.forEach(function (item, index, array) {
-                if (gamer.name == gamerArray[index].name && gamer.surname == gamerArray[index].surname) {
-                    output = {
-                        error: true,
-                        errCode: 503,
-                        message: 'The player was alredy existing'
-                    };
-                }
-            })
-            //Add new User to "Database" array
-            let newGamer = gamerArray.push(gamer);
+        //If valid parameters
+        if (!IsNull(req.body.name, req.body.surname, req.body.score)) {
+            //If player doesn't exist
+            if (!CheckIfExists(req.body.name, req.body.surname)) {
+                CreatePlayer(req.body);
+            }
         }
         // Send output
         res.send(output);
